5c373fb942ce54d5e5e4a051957e1b1ca7baed70
Valeriu Caraulean <valeriu.caraulean@git.ch>
2012-03-11 23:22:42 +0100

    Created solution and a domain class - Customer

---
+++ src/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace dnug.internaldsl
+{
+	public class Customer
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+		public string CreditCardNumber { get; set; }
+	}
+}
