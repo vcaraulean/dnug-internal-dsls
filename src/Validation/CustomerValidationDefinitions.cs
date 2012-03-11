425db273055e11d7f5b9ee1aa3a43d72361682f5
Valeriu Caraulean <valeriu.caraulean@git.ch>
2012-03-12 00:08:51 +0100

    Validation using fluent interface
    --HG--
    branch : using-fluent-configuration

---
+++ src/Validation/CustomerValidationDefinitions.cs
@@ -0,0 +1,15 @@
+﻿using NHibernate.Validator.Cfg.Loquacious;
+
+namespace dnug.internaldsl.Validation
+{
+	public class CustomerValidationDefinitions : ValidationDef<Customer>
+	{
+		public CustomerValidationDefinitions()
+		{
+			Define(x => x.FirstName).LengthBetween(3, 20);
+			Define(x => x.LastName).LengthBetween(3, 20);
+			Define(x => x.Age).Whitih(16, 65);
+			Define(x => x.CreditCardNumber).IsCreditCardNumber();
+		}
+	}
+}
