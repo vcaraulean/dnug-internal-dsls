91443b7f26958ee0570cefc762a94c18def024fc
Valeriu Caraulean <valeriu.caraulean@git.ch>
2012-03-11 23:36:02 +0100

    Validation of domain class using attributes
    --HG--
    branch : using-attributes

--- src/Customer.cs
+++ src/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using NHibernate.Mapping.Attributes;
+using NHibernate.Validator.Constraints;
 
 namespace dnug.internaldsl
 {
@@ -11,15 +12,19 @@ namespace dnug.internaldsl
 		public Guid Id { get; set; }
 
 		[Property(Column = "FIRST_NAME")]
+		[Length(Min = 3, Max = 20)]
 		public string FirstName { get; set; }
 
 		[Property]
+		[Length(Min = 3, Max = 20)]
 		public string LastName { get; set; }
 
 		[Property]
+		[Range(Min = 16, Max = 65)]
 		public int Age { get; set; }
 
 		[Property]
+		[CreditCardNumber]
 		public string CreditCardNumber { get; set; }
 	}
 }
