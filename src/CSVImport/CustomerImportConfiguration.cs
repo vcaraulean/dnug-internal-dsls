871956af4b22abb34d8f33e7a8cd5cc02ee832f5
Valeriu Caraulean <valeriu.caraulean@git.ch>
2012-03-12 00:17:54 +0100

    Configuring export using fluent api
    --HG--
    branch : using-fluent-configuration

---
+++ src/CSVImport/CustomerImportConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using CsvHelper.Configuration;
+
+namespace dnug.internaldsl.CSVImport
+{
+	public class CustomerImportConfiguration : CsvClassMap<Customer>
+	{
+		public CustomerImportConfiguration()
+		{
+			Map(x => x.Id).Name("Customer ID").Index(1);
+			Map(x => x.FirstName).Name("First name").Index(2);
+			Map(x => x.FirstName).Name("Last name").Index(3);
+
+			Map(x => x.Age).Ignore();
+			Map(x => x.CreditCardNumber).Ignore();
+		}
+	}
+}
