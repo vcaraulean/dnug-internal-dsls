7cc8b8d56cb3c77b5a47420be0ff2b93e98960c3
Valeriu Caraulean <valeriu.caraulean@git.ch>
2012-03-11 23:41:43 +0100

    CSV export configuration
    --HG--
    branch : using-attributes

--- src/Customer.cs
+++ src/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CsvHelper.Configuration;
 using NHibernate.Mapping.Attributes;
 using NHibernate.Validator.Constraints;
 
@@ -9,22 +10,27 @@ namespace dnug.internaldsl
 	{
 		[Id(Column = "CUSTOMER_ID")]
 		[Generator(1, Class = "guid")] 
+		[CsvField(Ignore = false, Name = "Customer ID", Index = 1)]
 		public Guid Id { get; set; }
 
 		[Property(Column = "FIRST_NAME")]
 		[Length(Min = 3, Max = 20)]
+		[CsvField(Index = 2, Name = "First name")]
 		public string FirstName { get; set; }
 
 		[Property]
 		[Length(Min = 3, Max = 20)]
+		[CsvField(Index = 3, Name = "Last name")]
 		public string LastName { get; set; }
 
 		[Property]
 		[Range(Min = 16, Max = 65)]
+		[CsvField(Ignore = true)]
 		public int Age { get; set; }
 
 		[Property]
 		[CreditCardNumber]
+		[CsvField(Ignore = true)]
 		public string CreditCardNumber { get; set; }
 	}
 }
