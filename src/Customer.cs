5d877b2992166633eefb13f234f1cf8e5b3b28b3
Valeriu Caraulean <valeriu.caraulean@git.ch>
2012-03-11 23:31:32 +0100

    Mapping domain class with attributes
    --HG--
    branch : using-attributes

--- src/Customer.cs
+++ src/Customer.cs
@@ -1,10 +1,25 @@
-﻿namespace dnug.internaldsl
+﻿using System;
+using NHibernate.Mapping.Attributes;
+
+namespace dnug.internaldsl
 {
+	[Class]
 	public class Customer
 	{
+		[Id(Column = "CUSTOMER_ID")]
+		[Generator(1, Class = "guid")] 
+		public Guid Id { get; set; }
+
+		[Property(Column = "FIRST_NAME")]
 		public string FirstName { get; set; }
+
+		[Property]
 		public string LastName { get; set; }
+
+		[Property]
 		public int Age { get; set; }
+
+		[Property]
 		public string CreditCardNumber { get; set; }
 	}
 }
